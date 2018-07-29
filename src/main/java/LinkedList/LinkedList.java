e3dcc7776e29e35daa61a22af8586301d660a912
Olek <oleksiy.mysko@gmail.com>
2018-07-29 09:51:55 +0200

    implements

---
+++ src/main/java/LinkedList/LinkedList.java
@@ -0,0 +1,40 @@
+package LinkedList;
+
+public class LinkedList {
+
+    private static  Node head;
+    private int size;
+
+    public LinkedList(Object data) {
+        head = new Node(data);
+    }
+
+
+    public void add(Object data){
+        Node copy = head;
+        head = new Node(data);
+        head.next = copy;
+        size ++;
+    }
+
+    public static void printList(){
+        Node temp = head;
+        while (temp!=null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    class Node {
+        private Object data;
+        private Node next;
+
+        public Node(Object data) {
+            this.data = data;
+        }
+
+        public Object getData() {
+            return data;
+        }
+    }
+}
---
+++ src/main/java/LinkedList/Main.java
@@ -0,0 +1,23 @@
+package LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList lista =new LinkedList(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
+        lista.add(10);
+        lista.add(11);
+        lista.add(12);
+        lista.add(13);
+        lista.add(14);
+        lista.add(15);
+        lista.add(16);
+        LinkedList.printList();
+    }
+}
